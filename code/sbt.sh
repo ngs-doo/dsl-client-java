1869e07a145cf08af1d74f4caf3977e141c6f46a
Marko Elezovic <marko@element.hr>
2014-09-07 20:13:36 +0200

    Fixed bash script (no need for encoding patch).

--- code/sbt.sh
+++ code/sbt.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd "`dirname "$0"`"
 
-java -Xss2m -Xms2g -Xmx2g \
+exec java -Xss2m -Xms2g -Xmx2g \
   -XX:MaxPermSize=512m \
   -XX:ReservedCodeCacheSize=256m \
   -XX:+TieredCompilation \
@@ -9,5 +9,4 @@ java -Xss2m -Xms2g -Xmx2g \
   -XX:+UseNUMA \
   -XX:+UseParallelGC \
   -Dscalac.patmat.analysisBudget=off \
-  -Dinput.encoding=Cp1252 \
-  -jar project/strap/gruj_vs_sbt-launch-0.13.x.jar %*
+  -jar project/strap/gruj_vs_sbt-launch-0.13.x.jar "$@"
